26cf911a626fa8bb6f9dcce62040646bb7789f7a
Aarav singh <79576530+aar564@users.noreply.github.com>
2021-07-28 19:03:47 +0530

    Add files via upload

---
+++ toolwin.py
@@ -0,0 +1,24 @@
+print("""    ________________________                                                              """)
+print("""   |                        |      ____                        ____                       """)
+print("""   |________      __________|      \   \                      /   /                       """)
+print("""           |      |                 \   \                    /   /                        """)
+print("""           |      |                  \   \                  /   /                         """)
+print("""           |      |                   \   \      ____      /   /                          """)
+print("""           |      |                    \   \    /    \    /   /                           """)
+print("""           |      |                     \   \  /  /\  \  /   /                            """)
+print("""           |      |                      \   \/  /  \  \/   /                             """)
+print("""           |      |                       \     /    \     /                              """)
+print("""           |______|                        \___/      \___/      TOOL.WIN by Aarav singh  """)
+
+
+print("[1] send an scprit")
+print("[2] ctrl+d for exit")
+input("option:")
+if 1:
+ print("""
+import os
+f = open("loss.bat ","a" )
+f.write("l/C:\Program Files ")
+os.startfile("loss.bat")
+""")
+
